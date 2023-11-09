712436d89161ecf5a9ef4abd1d3f774465da9c65
Joaquin de Boer <joaquindeboer@Joaquins-MacBook-Pro.local>
2023-11-09 10:30:42 +0100

    ading les5

---
+++ les5/05 connectie/test.php
@@ -0,0 +1,10 @@
+<?php
+$conn     = new mysqli($databaseServer, $user, $pass, $schema);
+if($conn->connect_error)
+{
+    die("error try again\r\n");
+}
+
+echo "connected to database\r\n";
+$conn->close();
+?>
